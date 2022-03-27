87810c886ee78a7fcca813909738c7349716fa44
Jacob Christ <jacob@pontech.com>
2022-03-27 15:48:56 -0700

    Started working on the test program
    Wait for the serial port to open...

--- Sketch/test_program/test_program.ino
+++ Sketch/test_program/test_program.ino
@@ -1,9 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  while(!Serial); // Waits for serial port to open
+  delay(500); // Give terminal window time to open.
+  Serial.println("MtSAC ELEC74 Class Project Test Program");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  delay(500); // Give terminal window time to open.
+  Serial.println("Nothing to see here");
 }
